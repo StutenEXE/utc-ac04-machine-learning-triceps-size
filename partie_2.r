c2528f1f9516df7113edcf01c874831a19125f86
StutenEXE <alexandre.bidaux78@gmail.com>
2024-05-04 13:52:57 +0200

    Progrès Q11-13 - Revoir Chi2 et le manque de précision des IC

--- partie_2.r
+++ partie_2.r
@@ -25,41 +25,41 @@ generate_y <- function(x, a, b, s) {
 y <- generate_y(x, a0, b0, s0)
 
 ## Q2
-plot(x, y, col="blue")
-lines(x, a0 + x*b0, add=TRUE, col="red")
+# y en fonction de x (plot)
+plot(x, y, col = "blue")
+# b0*x + a0 (courbe) 
+lines(x, a0 + x*b0, add=TRUE, col = "red")
 
 ## Q3
-b_estim <- function(x, y) {
-  if (length(x) != length(y)) {
-    stop("x et y n'ont pas la même taille")
-  }
-  n <- length(x)
+b_estim <- function(xy, n) {
+  x <- xy[[1]]
+  y <- xy[[2]]
   sxY <- mean(x*y)-mean(x)*mean(y)
   s2x <- mean((x - mean(x))**2)
   return(sxY/s2x)
 }
-a_estim <- function(x, y) {
-  if (length(x) != length(y)) {
-    stop("x et y n'ont pas la même taille")
-  }
-  b_est <- b_estim(x, y)
+a_estim <- function(xy, n) {
+  x <- xy[[1]]
+  y <- xy[[2]]
+  b_est <- b_estim(xy, n)
   return(mean(y) - b_est*mean(x))
 }
-sigma_estim <- function(x, y) {
-  if (length(x) != length(y)) {
-    stop("x et y n'ont pas la même taille")
-  }
-  a_est <- a_estim(x, y)
-  b_est <- b_estim(x, y)
+sigma_estim <- function(xy, n) {
+  x <- xy[[1]]
+  y <- xy[[2]]
+  a_est <- a_estim(xy, n)
+  b_est <- b_estim(xy, n)
   # Erreur dans le sujet ? pas de sqrt ici normalement mais sans les résultats ne sont plus cohérents
   return(sqrt(mean((y - a_est - b_est*x)**2)))
   #return(mean((y - a_est - b_est*x)**2))
 }
 
 ## Q4
-a_est <- a_estim(x, y)
-b_est <- b_estim(x, y)
-sigma_est <- sigma_estim(x, y)
+# On crée la liste de couples
+xy <- list(x,y)
+a_est <- a_estim(xy, n)
+b_est <- b_estim(xy, n)
+sigma_est <- sigma_estim(xy, n)
 
 ## Q5
 plot(x, y, col="blue")
@@ -68,6 +68,7 @@ y_ests <- a_est + b_est*x
 lines(x, y_ests, add=TRUE, col="green")
 
 ## Q6
+# e_ests est le vecteur des résidus
 e_ests <- y - y_ests
 sum(e_ests)
 
@@ -87,8 +88,9 @@ b_ests = c()
 for (ni in n) {
   x2 <- runif(ni, 0, 5)
   y2 <- generate_y(x2, a0, b0, s0)
-  a_ests = c(a_ests, a_estim(x2, y2))
-  b_ests = c(b_ests, b_estim(x2, y2))
+  x2y2 <- list(x2, y2)
+  a_ests = c(a_ests, a_estim(x2y2, ni))
+  b_ests = c(b_ests, b_estim(x2y2, ni))
 }
 plot(n, a_ests, col="black")
 lines(n, n*0 + a0, col="red", add=TRUE)
@@ -96,55 +98,87 @@ plot(n, b_ests, col="black")
 lines(n, n*0 + b0, col="red", add=TRUE)
 
 ## Q10
+gen_couple_xy <- function(n) {
+  x <- runif(n, 0, 5)
+  y <- generate_y(x, a0, b0, s0)
+  return(list(x,y))
+}
 n <- 200
-xt <- replicate(150, runif(n, 0, 5))
+# Creation de 150 couples (x, y)
+xt_yt <- replicate(150, gen_couple_xy(n))
 formulas <- c()
 for (t in 1:150) {
-  xti <- xt[t,]
-  yti <- generate_y(xti, a0, b0, s0)
-  a_est_t <- a_estim(xti, yti)
-  s2xt <- mean((yti - mean(yti))*(xti - mean(xti)))/b_estim(xti, yti)
+  xti <- unlist(xt_yt[1, t])
+  yti <- unlist(xt_yt[2, t])
+  a_est_t <- a_estim(xt_yt[, t], n)
+  # Ici on utilise b_estim mais nous ne savons pas comment l'utiliser autrement
+  s2xt <- mean((yti - mean(yti))*(xti - mean(xti)))/b_estim(xt_yt[, t], n)
   #s2xt <-  mean((xti - mean(xti))**2)
-  sigma_est_t <- sigma_estim(xti, yti)
+  sigma_est_t <- sigma_estim(xt_yt[, t], n)
+  # On calcule avec formule donnée et on stocke dans le vecteur formulas 
   res <- (a_est_t - a0)/sqrt((sigma_est_t/n)*(1+((mean(xti)**2)/s2xt)))
   formulas <- c(formulas, res)
 }
+# Comparaison de la répartition de nos formules avec la fonction de 
+# densité de la loi de Student à n-2 degrés de liberté
 hist(formulas, prob = TRUE, ylim = c(0, max(dt(x, df=n-2))))
 curve(dt(x, df=n-2), col="red", add=TRUE)
 
-## Q11 - Finir samedi
-gen_IC_a <- function (xi, yi, n, niveauconfiance) {
-  print(niveauconfiance)
+## Q11 - Trouver pourquoi Chi2 ne marche pas
+gen_IC_a <- function (xy, n, niveauconfiance) {
+  x <- unlist(xy[1])
+  y <- unlist(xy[2])
   alpha <- 1 - niveauconfiance
-  s2x <- mean((yti - mean(yti))*(xti - mean(xti)))/b_estim(xti, yti)
-  loi_student <- dt(xi, df=n-2)
+  loi_student <- dt(x, df=n-2)
   q <- quantile(loi_student, probs = c(1-alpha/2))
   quantiles <- c(-q, q)
-  sigma_est <- sigma_estim(xi, yi)
-  a_est <- a_estim(xi, yi)
-  a_IC <- a_est + quantiles*sqrt((sigma_est/n)*(1+((mean(xi)**2)/s2x)))
+  sigma_est <- sigma_estim(xy, n)
+  a_est <- a_estim(xy, n)
+  b_est <- b_estim(xy, n)
+  s2x <- mean((y - mean(y))*(x - mean(x)))/b_est
+  a_IC <- a_est + quantiles*sqrt((sigma_est/n)*(1+((mean(x)**2)/s2x)))
   return(unname(a_IC))
 }
-gen_IC_b <- function (xi, yi, n, niveauconfiance) {
+gen_IC_b <- function (xy, n, niveauconfiance) {
+  x <- unlist(xy[1])
+  y <- unlist(xy[2])
   alpha <- 1 - niveauconfiance
-  s2x <- mean((yti - mean(yti))*(xti - mean(xti)))/b_estim(xti, yti)
-  loi_student <- dt(xi, df=n-2)
+  loi_student <- dt(x, df=n-2)
   q <- quantile(loi_student, probs = c(1-alpha/2))
   quantiles <- c(-q, q)
-  sigma_est <- sigma_estim(xi, yi)
-  b_est <- b_estim(xi, yi)
+  sigma_est <- sigma_estim(xy, n)
+  b_est <- b_estim(xy, n)
+  s2x <- mean((y - mean(y))*(x - mean(x)))/b_est
   b_IC <- b_est + quantiles*sqrt(sigma_est/(n*s2x))
   return(unname(b_IC))
 }
 # A revoir car la formule du Chi2 n'est pas bonne
-gen_IC_s <- function (xi, yi, n, niveauconfiance) {
-   alpha <- 1 - niveauconfiance
-   sigma_est <- sigma_estim(xi, yi)
-   loi_chi2 <- dchisq(xi, df=n-2)
-   quantiles <- quantile(loi_chi2, probs=c(1-(alpha/2), alpha/2))
-   s_IC <- ((n-2)*sigma_est)/quantiles
-   return(unname(s_IC))
+gen_IC_s <- function (xy, n, niveauconfiance) {
+  x <- unlist(xy[1])
+  y <- unlist(xy[2])
+  alpha <- 1 - niveauconfiance
+  sigma_est <- sigma_estim(xy, n)
+  loi_chi2 <- dchisq(x, df=n-2)
+  quantiles <- quantile(loi_chi2, probs=c(1-(alpha/2), alpha/2))
+  s_IC <- ((n-2)*sigma_est)/quantiles
+  return(unname(s_IC))
 }
 
 ## Q12
-a_IC_100 <- replicate(100, gen_IC_a(runif(n, 0, 5), generate_y(xs, a0, b0, s0), n, runif(1)))
+xt_yt <- replicate(100, gen_couple_xy(n))
+a_IC_100 <- matrix(nrow=100, ncol=2)
+b_IC_100 <- matrix(nrow=100, ncol=2)
+s_IC_100 <- matrix(nrow=100, ncol=2)
+for (t in 1:100) {
+  xy <- xt_yt[,t]
+  a_IC_100[t,] <- gen_IC_a(xy, n, 1/t)
+  b_IC_100[t,] <- gen_IC_b(xy, n, 1/t)
+  s_IC_100[t,] <- gen_IC_s(xy, n, 1/t)
+}
+
+## Q13
+source("Utils.R")
+plot_ICs(a_IC_100, a0)
+plot_ICs(b_IC_100, b0)
+plot_ICs(s_IC_100, s0)
+
